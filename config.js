be2377d64b5bd46966287bc8c88629dbdb33b667
GermanEngineering <135478370+FritzTheGerman@users.noreply.github.com>
2026-04-23 16:54:46 -0500

    Add configuration for personnel and report ranges

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PERSONNEL_RANGE: 'A:G',
+  PUNISHMENTS_RANGE: 'Punishments!A:H',
+  EVENTS_RANGE: 'Events!A:K',
+  REPORTS_RANGE: 'Reports!A:K',
+  RANK_HISTORY_RANGE: 'Rank History!A:J'
+};

d9128fa1271faafd072fcc25fc3d0e8fbba6b1a8
GermanEngineering <135478370+FritzTheGerman@users.noreply.github.com>
2026-04-23 16:55:42 -0500

    Implement Google Sheets utility functions

---
+++ utils/sheets.js
@@ -0,0 +1,57 @@
+const { google } = require('googleapis');
+
+const sheets = new google.sheets({
+  version: 'v4',
+  auth: new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  }),
+});
+
+async function getRows(range) {
+  const res = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range,
+  });
+
+  return res.data.values || [];
+}
+
+async function appendRow(range, data) {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range,
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: [data],
+    },
+  });
+}
+
+async function updateRow(range, data) {
+  await sheets.spreadsheets.values.update({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range,
+    valueInputOption: 'USER_ENTERED',
+    requestBody: {
+      values: [data],
+    },
+  });
+}
+
+async function clearRange(range) {
+  await sheets.spreadsheets.values.clear({
+    spreadsheetId: process.env.GOOGLE_SHEET_ID,
+    range,
+  });
+}
+
+module.exports = {
+  getRows,
+  appendRow,
+  updateRow,
+  clearRange
+};

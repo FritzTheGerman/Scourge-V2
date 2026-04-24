69b5eb478f1c022d9c9a7f4393314ef97e49f350
GermanEngineering <135478370+FritzTheGerman@users.noreply.github.com>
2026-04-23 20:36:13 -0500

    Update personnel range and add new config entries

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  PERSONNEL_RANGE: 'A:G',
+  PERSONNEL_RANGE: 'A:H',
   PUNISHMENTS_RANGE: 'Punishments!A:H',
   EVENTS_RANGE: 'Events!A:K',
   REPORTS_RANGE: 'Reports!A:K',
   RANK_HISTORY_RANGE: 'Rank History!A:J',
   COMMAND_LOGS_RANGE: 'Command Logs!A:J',
-  ADMIN_ROLES_RANGE: 'Admin Roles!A:E'
+  ADMIN_ROLES_RANGE: 'Admin Roles!A:E',
+  PENDING_VERIFICATIONS_RANGE: 'Pending Verifications!A:F'
 };

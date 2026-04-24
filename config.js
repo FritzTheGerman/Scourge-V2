1e3187f164666e2c114bb4b19959343866edd290
GermanEngineering <135478370+FritzTheGerman@users.noreply.github.com>
2026-04-23 19:03:06 -0500

    Add COMMAND_LOGS_RANGE to config.js

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ module.exports = {
   PUNISHMENTS_RANGE: 'Punishments!A:H',
   EVENTS_RANGE: 'Events!A:K',
   REPORTS_RANGE: 'Reports!A:K',
-  RANK_HISTORY_RANGE: 'Rank History!A:J'
+  RANK_HISTORY_RANGE: 'Rank History!A:J',
+  COMMAND_LOGS_RANGE: 'Command Logs!A:H'
 };
